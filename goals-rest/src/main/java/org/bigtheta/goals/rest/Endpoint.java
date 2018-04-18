daa94b77da02c049e36cfd4b4ba877b711c6f016
bigtheta-dev <bigtheta.org@gmail.com>
2018-04-18 13:01:53 +0300

    add bundled jetty

--- goals-rest/src/main/java/org/bigtheta/goals/rest/Endpoint.java
+++ goals-rest/src/main/java/org/bigtheta/goals/rest/Endpoint.java
@@ -1,8 +1,16 @@
 package org.bigtheta.goals.rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
 public class Endpoint {
-    public String get(){
+    @GET
+    @Path("test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String get() {
         return "result";
     }
-
 }
