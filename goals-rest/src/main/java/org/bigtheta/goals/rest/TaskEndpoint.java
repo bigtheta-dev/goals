945ee919c454cd29de3d1392cbae851c3c029600
bigtheta-dev <bigtheta.org@gmail.com>
2018-04-26 11:50:49 +0300

    cors support

--- goals-rest/src/main/java/org/bigtheta/goals/rest/TaskEndpoint.java
+++ goals-rest/src/main/java/org/bigtheta/goals/rest/TaskEndpoint.java
@@ -1,13 +1,13 @@
 package org.bigtheta.goals.rest;
 
 import org.bigtheta.goals.core.JsonConverter;
-import org.bigtheta.goals.repo.Repo;
-import org.bigtheta.goals.repo.impl.inMemory.InMemoryRepo;
 import org.bigtheta.goals.core.Task;
+import org.bigtheta.goals.repo.Repo;
 import org.bigtheta.goals.repo.impl.postgres.PgRepo;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/api/task")
 public class TaskEndpoint {
@@ -21,40 +21,58 @@ public class TaskEndpoint {
     @GET
     @Path("")
     @Produces(MediaType.APPLICATION_JSON)
-    public String list() {
-        return new JsonConverter().marchall(repo.list());
+    public Response list() {
+        String entity = new JsonConverter().marchall(repo.list());
+        return buildResponse(entity);
     }
 
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String read(@PathParam("id") String id) {
-        return new JsonConverter().marchall(repo.read(id));
+    public Response read(@PathParam("id") String id) {
+        String entity = new JsonConverter().marchall(repo.read(id));
+        return buildResponse(entity);
     }
 
     @DELETE
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String delete(@PathParam("id") String id) {
-        return new JsonConverter().marchall(repo.delete(id));
+    public Response delete(@PathParam("id") String id) {
+        String entity = new JsonConverter().marchall(repo.delete(id));
+        return buildResponse(entity);
     }
 
     @POST
     @Path("")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String create(String input) {
+    public Response create(String input) {
         Task task = new JsonConverter().unmarchall(input);
-        return new JsonConverter().marchall(repo.create(task));
+        String entity = new JsonConverter().marchall(repo.create(task));
+        return buildResponse(entity);
     }
 
     @PUT
     @Path("")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String update(String input) {
+    public Response update(String input) {
         Task task = new JsonConverter().unmarchall(input);
-        return new JsonConverter().marchall(repo.update(task));
+        String entity = new JsonConverter().marchall(repo.update(task));
+        return buildResponse(entity);
+    }
+
+    private Response buildResponse(String entity) {
+        return Response
+                .status(200)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Headers",
+                        "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Methods",
+                        "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .entity(entity)
+                .build();
     }
 
 }
